1ccd85711bf84c89f19fb4a965a0e06605ddb07a
Akash Limbu <akashwanem@hotmail.com>
2021-12-16 19:53:43 +0000

    done

---
+++ Unsolved/script.js
@@ -0,0 +1,8 @@
+//WRITE YOUR CODE BELOW
+let students: ['bob', 'harry', 'finn', 'lewis', 'charlie'];
+
+console.log(students.length);
+
+for (i = 0; i < students.length; i++) {
+    console.log('Great to see you, ' + students[i]);
+} 
